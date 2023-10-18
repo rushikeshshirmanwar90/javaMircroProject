53b983630e357d20bde7b3aa23d556b9f20e4e75
= <=>
2023-10-18 19:57:52 +0530

    adding files

---
+++ ClientEnd.java
@@ -0,0 +1,106 @@
+package javaMircroProject;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.net.*;
+import java.io.*;
+
+public class ClientEnd {
+	public JFrame frame;
+	private JTextField textField;
+    private static JTextArea textArea;
+    private static Socket con;
+    DataInputStream input;
+    DataOutputStream output;
+    private JScrollPane scrollPane;
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientEnd window = new ClientEnd();
+					window.frame.setVisible(true);
+				
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		con = new Socket("127.0.0.1", 8080);
+		while (true) {
+			try {
+				DataInputStream input = new DataInputStream(con.getInputStream());
+				String string = input.readUTF();
+				textArea.setText(textArea.getText() + "\n" + "User 1 : " + string);
+			} catch (Exception ev) {
+				textArea.setText(textArea.getText()+" \n" +"Network issues ");
+				try {
+						Thread.sleep(2000);
+						System.exit(0);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+			}
+		}
+	}
+
+	public ClientEnd() {
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(UIManager.getColor("MenuBar.highlight"));
+		frame.setBounds(100, 100, 605, 378);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.setTitle("Client End : Chat Application");
+
+		textField = new JTextField();
+		textField.setFont(new Font("Lato Medium", Font.PLAIN, 22));
+		textField.setForeground(Color.white);
+		textField.setBackground(Color.darkGray);
+		textField.setBounds(12, 45, 344, 38);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+
+		JButton btnNewButton = new JButton("Send");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (textField.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "Please write some text !");
+				}else  {
+					textArea.setText(textArea.getText() + "\n" + "User 2 : " + textField.getText());
+					try {
+						DataOutputStream output = new DataOutputStream(con.getOutputStream());
+						output.writeUTF(textField.getText());
+					} catch (IOException e1) {
+						textArea.setText(textArea.getText() + "\n " + " Network issues");
+						try {
+							Thread.sleep(2000);
+							System.exit(0);
+						} catch (InterruptedException e2) {
+							e2.printStackTrace();
+						}
+					}
+					textField.setText("");
+				}
+			}
+		});
+
+		btnNewButton.setFont(new Font("Georgia", Font.BOLD, 22));
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBackground(Color.gray);
+		btnNewButton.setBounds(398, 45, 164, 38);
+		frame.getContentPane().add(btnNewButton);
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(12, 149, 557, 157);
+		frame.getContentPane().add(scrollPane);
+		textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		textArea.setFont(new Font("Comic Sans MS", Font.PLAIN, 22));
+		textArea.setForeground(Color.decode("#FFFFFF"));
+		textArea.setBackground(Color.decode("#333333"));
+	}
+}
